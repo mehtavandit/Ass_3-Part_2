f63e8e2f6e3a87cf6837f08366c8870163066219
akashamba <akashambashankar@gmail.com>
2023-04-10 07:23:50 -0400

    all methods completed in expandable array

--- ExpandableArray.java
+++ ExpandableArray.java
@@ -3,7 +3,7 @@ public class ExpandableArray {
         long key;
         String value;
 
-        Node(long key, String value) {
+        Node (long key, String value) {
             this.key = key;
             this.value = value;
         }
@@ -21,20 +21,52 @@ public class ExpandableArray {
     }
 
     public void insert(long key, String value) {
-        if (top+1 == arr.length)
+        if (top == arr.length)
             expand();
-        arr[top] = new Node(key, value);
-        top++;
+        
+        if (getIndex(key) == -1) {
+            arr[top] = new Node(key, value);
+            top++;
+        } else {
+            System.out.println("Key already exists");
+        }
     }
 
-    public String search(long key) {
+    public void search(long key) {
         for (int i=0; i<top; i++) {
             if (arr[i].key == key) {
-                return arr[i].value;
+                System.out.println(arr[i].value);
+                return;
             }
         }
 
-        return "-1";
+        System.out.println("Not found");
+    }
+
+    private int getIndex(long key) {
+        for (int i=0; i<top; i++) {
+            if (arr[i].key == key) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public long predecessor(long key) {
+        int index=getIndex(key);
+
+        if (index!=-1 && arr[index].key == key && index-1>=0) {
+            return arr[index-1].key;
+        } else return -1;
+    }
+
+    public long successor(long key) {
+        int index=getIndex(key);
+
+        if (index!=-1 && arr[index].key == key && index+1<top) {
+            return arr[index+1].key;
+        } else return -1;
     }
 
     public int size() {
@@ -45,5 +77,113 @@ public class ExpandableArray {
         Node[] temp = new Node[Math.min(1000, 2*arr.length)];
         System.arraycopy(arr, 0, temp, 0, arr.length);
         arr = temp;
+    }
+
+    private long[] merge(long[] keys, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+ 
+        long L[] = new long[n1];
+        long R[] = new long[n2];
+ 
+        for (int i = 0; i < n1; ++i)
+            L[i] = keys[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = keys[m + 1 + j];
+ 
+        int i = 0, j = 0;
+ 
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                keys[k] = L[i];
+                i++;
+            }
+            else {
+                keys[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+ 
+        while (i < n1) {
+            keys[k] = L[i];
+            i++;
+            k++;
+        }
+ 
+        while (j < n2) {
+            keys[k] = R[j];
+            j++;
+            k++;
+        }
+        return keys;
+    }
+
+    long[] sortKeys(long keys[], int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            sortKeys(keys, l, m);
+            sortKeys(keys, m + 1, r);
+            merge(keys, l, m, r);
+        }
+
+        return keys;
+    }
+
+    public void displaySortedKeys() {
+        long[] keys = new long[top];
+        for (int i = 0; i < top; i++) {
+            keys[i] = arr[i].key;
+        }
+
+        long[] sortedKeys = sortKeys(keys, 0, keys.length-1);
+        for (int i = 0; i < sortedKeys.length; i++) {
+            System.out.print(sortedKeys[i]+" ");
+            search(sortedKeys[i]);
+        }
+    }
+
+    public void delete(long key) {
+        int index = getIndex(key);
+
+        if (index != -1) {
+            for (int i = index; i < top-1; i++) {
+                arr[i] = arr[i+1];
+            }
+            top--;
+        } else {
+            System.out.println("No such key found");
+        }
+    }
+
+    public int range(long key1, long key2) {
+        int counter = 0;
+        
+        for (int i = 0; i < top; i++) {
+            if (arr[i].key > key1 && arr[i].key < key2)
+                counter++;
+        }
+
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        ExpandableArray ex = new ExpandableArray(3);
+        ex.insert(3, "world");
+        ex.insert(2, "Hi");
+        ex.insert(1, "Hello");
+        ex.insert(1, "Hello");
+        ex.insert(10, "Hello");
+        ex.insert(12, "Hello");
+        ex.insert(123, "Hello");
+        ex.insert(221, "Hello");
+        ex.displaySortedKeys();
+        ex.delete(4);
+        ex.displaySortedKeys();
+        System.out.println(ex.successor(0));
+
+        System.out.println("");
+        System.out.println(ex.range(1, 12));
     }
 }
