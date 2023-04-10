27618f99aca2ecc3a9007a066d517c6c25b2e9a5
mehtavandit <vanditmehta303@gmail.com>
2023-04-10 15:46:04 -0400

    Initial Commit

---
+++ AVL.java
@@ -0,0 +1,208 @@
+public class AVL {
+    BinaryNode root;
+
+    public AVL(){
+        root = null;
+    }
+
+    //Preorder
+
+    public void preorder(BinaryNode node)
+    {
+        if(node == null){
+            return;
+        }
+        System.out.print(node.value+" ");
+        preorder(node.left);
+        preorder(node.right);
+    }
+
+    //Inorder
+
+    public void inorder(BinaryNode node)
+    {
+        if(node == null){
+            return;
+        }
+        inorder(node.left);
+        System.out.print(node.value+" ");
+        inorder(node.right);
+    }
+
+    //Preorder
+
+    public void postorder(BinaryNode node){
+        if(node == null){
+            return;
+        }
+        postorder(node.left);
+        postorder(node.right);
+        System.out.print(node.value+" ");
+    }
+
+    public boolean search(BinaryNode node, int value)
+    {
+        if(node == null){
+            return false;
+        }
+
+        boolean isPresent = false;
+
+        while(node!=null)
+        {
+            if(value<node.value){
+                node = node.left;
+            }
+            else if(value>node.value){
+                node = node.right;
+            }else{
+                isPresent = true;
+                break;
+            }
+        }
+        return isPresent;
+    }
+
+    //get height
+    public int getHeight(BinaryNode node){
+        if(node == null){
+            return 0;
+        }
+        return node.height;
+    }
+
+    // rotate right
+    private BinaryNode rotateRight(BinaryNode disbalancednode){
+        BinaryNode newRoot = disbalancednode.left;
+        disbalancednode.left = disbalancednode.left.right;
+        newRoot.right = disbalancednode;
+        disbalancednode.height = 1 + Math.max(getHeight(disbalancednode.left), getHeight(disbalancednode.right));
+        newRoot.height = 1 + Math.max(getHeight(newRoot.left), getHeight(newRoot.right));
+        return newRoot;
+    }
+
+    // rotate left
+    private BinaryNode rotateLeft(BinaryNode disbalancednode){
+        BinaryNode newRoot = disbalancednode.right;
+        disbalancednode.right = disbalancednode.right.left;
+        newRoot.left = disbalancednode;
+        disbalancednode.height = 1 + Math.max(getHeight(disbalancednode.left), getHeight(disbalancednode.right));
+        newRoot.height = 1 + Math.max(getHeight(newRoot.left), getHeight(newRoot.right));
+        return newRoot;
+    }
+
+    //getBalance
+    public int getBalance(BinaryNode node){
+        if(node == null){
+            return 0;
+        }
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    //insert method
+    private BinaryNode insertNode(BinaryNode node, int value){
+        if(node == null){
+            BinaryNode newNode = new BinaryNode();
+            newNode.value = value;
+            newNode.height = 1;
+            return newNode;
+        }
+        else if(value<node.value){
+            node.left = insertNode(node.left, value);
+        }
+        else{
+            node.right = insertNode(node.right, value);
+        }
+
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+        int balance = getBalance(node);
+
+        if(balance>1 && value < node.left.value){ //LL
+            return rotateRight(node);
+        }
+
+        if(balance>1 && value>node.left.value){ //LR
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+
+        if(balance<-1 && value>node.right.value){ //RR
+            return rotateLeft(node);
+        }
+
+        if(balance<-1 && value<node.right.value){ //RL
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
+        }
+
+        return node;
+    }
+
+    public void insert(int value){
+        root = insertNode(root,value);
+    }
+
+    //Minimum node
+    public static BinaryNode minimumNode(BinaryNode node){
+        if(node.left!=null){
+            return node;
+        }
+        return minimumNode(node.left);
+    }
+
+    //Delete node
+    public BinaryNode deleteNode(BinaryNode node, int value){
+        if(node == null){
+            System.out.println("Value not found in AVL Tree");
+            return node;
+        }
+        if(value<node.value){
+            node.left = deleteNode(node.left, value);
+        }
+        else if(value>node.value){
+            node.right = deleteNode(node.right, value);
+        }
+        else{
+            if(node.left!=null && node.right!=null){
+                BinaryNode temp = node;
+                BinaryNode minNodeForRight = minimumNode(temp.right);
+                node.value = minNodeForRight.value;
+                node.right = deleteNode(node.right, minNodeForRight.value);
+            }
+            else if(node.left!=null){
+                node = node.left;
+            }
+            else if(node.right!=null){
+                node = node.right;
+            }else{
+                node = null;
+            }
+        }
+        int balance = getBalance(node);
+
+        if(balance>1 && getBalance(node.left)>=0){ //LL
+            return rotateRight(node);
+        }
+
+        if(balance>1 && getBalance(node.left)<0){ //LR
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+
+        if(balance<-1 && getBalance(node.right)<=0){ //RR
+            return rotateLeft(node);
+        }
+
+        if(balance<-1 && getBalance(node.right)>0){ //RL
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
+        }
+        return node;
+
+    }
+
+    public void delete(int value){
+        root = deleteNode(root,value);
+    }
+
+}
---
+++ BinaryNode.java
@@ -0,0 +1,11 @@
+public class BinaryNode {
+    int value;
+    int height;
+    BinaryNode left;
+    BinaryNode right;
+
+    BinaryNode() {
+        this.height = 0;
+    }
+
+}
